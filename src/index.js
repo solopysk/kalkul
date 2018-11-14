6b9498ec286a47470a19415903408691e76c3120
solopysk <43399651+solopysk@users.noreply.github.com>
2018-11-14 09:18:39 +0100

    h

--- src/index.js
+++ src/index.js
@@ -9,12 +9,34 @@ function solveExpression() {
 function toPostfix(str) {
   var postfix = "";
   var operands = [];
+  var operators = [];
   for (let a = 0; a < str.length; a++) {
-    if (str[a].isNaN()) {
+    if (isNaN(str[a])) {
+      operators.push(str[a]);
     } else {
       postfix += str[a];
+      operands.push(str[a]);
+    }
+    if (operands.length > 1) {
+      for (let b = 0; b < operators.length; b++) {
+        if (Weight(operators[b]) > 1) {
+          postfix += operators[b];
+          console.log(operators);
+          operators.splice(operators[b], 1);
+        }
+        // else if (operands.length > 0) {
+        //  postfix += operators.pop();
+        //}
+        operands.splice(2);
+        // operands.splice(1);
+      }
     }
   }
+
+  for (let c = 0; c < operators.length; c++) {
+    postfix += operators[c];
+  }
+  //console.log(operands);
   console.log(postfix);
 }
 function Weight(str) {
