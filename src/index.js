133253ff5acc2379bdd50669515fac1a1efb29dd
solopysk <43399651+solopysk@users.noreply.github.com>
2018-11-14 20:51:13 +0100

    next

--- src/index.js
+++ src/index.js
@@ -1,95 +1,135 @@
 function solveExpression() {
-    var vyraz = document.getElementById("vyraz").value;
-    var stack = [];
-    var output = "";
-    var str = vyraz.split("");
-    console.log(str);
-    vyraz = toPostfix(str);
-    return vyraz;
+   var vyraz = document.getElementById("vyraz").value;
+   var stack = [];
+   var output = "";
+   var str = vyraz.split("");
+   vyraz = toPostfix(str);
+   return vyraz;
 }
 function toPostfix(str) {
-    var postfix = "";
-    var operands = [];
-    var operators = [];
-    var count = 0;
-    for (let a = 0; a < str.length; a++) {
-        if (isNaN(str[a])) {
-            if (Weight(str[a]) > 1 && operands.length > 1) {
-                if (operands.length > 2) {
-                    alert("cau");
-                    var lastChar = postfix[postfix.length - 1];
-                    postfix[postfix.length - 1] = str[a];
-                    postfix += lastChar;
-                } else {
-                    postfix += str[a];
-                }
-                console.log(postfix);
-                alert();
+   var postfix = "";
+   var operands = [];
+   var operators = [];
+   var count = 0;
+   var more = true;
+   var operandses = false;
+   var precedence = false;
+   for (let a = 0; a < str.length; a++) {
+      if (isNaN(str[a])) {
+         if (a == 0 || operandses === true) {
+            return "error";
+         }
+         operandses = true;
+         if (Weight(str[a]) > 1 && operands.length > 1) {
+            if (operands.length > 2) {
+               alert("cau");
+               var lastChar = postfix[postfix.length - 1];
+               postfix[postfix.length - 1] = str[a];
+               postfix += lastChar;
+               operators.push(str[a]);
             } else {
-                operators.push(str[a]);
+               postfix += str[a];
+               operators.push(str[a]);
+               console.log(operators);
             }
-        } else {
-            postfix += str[a];
-            operands.push(str[a]);
-        }
-        if (operands.length > 1 && operators.length < 2) {
-            for (let a = 0; a < operators.length; a++) {
-                if (Weight(operators[a]) > 1) {
-                    postfix += operators.pop();
-                    count++;
-                }
+         } else {
+            operators.push(str[a]);
+         }
+         more = false;
+      } else {
+         postfix += str[a];
+         operands.push(str[a]);
+         more = true;
+         operandses = false;
+      }
+      console.log(operators[operators.length - 1]);
+      console.log(operators);
+      if (
+         operands.length > 2 &&
+         operators.length > 1 &&
+         Weight(operators[operators.length - 1]) > 1
+      ) {
+         alert("KE");
+         postfix += str[a];
+         operators.pop();
+         operands.pop();
+         operands.pop();
+      } else if (operands.length > 1 && operators.length < 2 && more == true) {
+         for (let a = 0; a < operators.length; a++) {
+            if (Weight(operators[a]) > 1) {
+               postfix += operators.pop();
             }
-            operands.pop();
-        } else if (operands.length > 2 && operators.length > 1) {
-            alert();
-            for (let b = 0; b < operators.length; b++) {
-                console.log("JEDEME");
-                if (Weight(operators[b]) > 1) {
-                    postfix += operators[b];
+         }
+         operands.pop();
+      } else if (operands.length > 2 && operators.length > 1 && more == true) {
+         for (let b = 0; b < operators.length; b++) {
+            if (Weight(operators[b]) > 1) {
+               postfix += operators[b];
 
-                    operators.splice(operators.indexOf(operators[b]), 1);
-                }
-                operands.splice(2);
+               operators.splice(operators.indexOf(operators[b]), 1);
             }
-        } else {
-        }
-    }
-    console.log(postfix);
-
-    for (let c = 0; c < operators.length; c++) {
-        if (Weight(operators[c]) > 1) {
-            console.log(Weight(operators[c]));
-            console.log(operators[c]);
-            postfix += operators[c];
-            operators.splice(operators.indexOf(operators[c]), 1);
-        }
-    }
-    //  console.log(operators);
-    for (let c = 0; c < operators.length; c++) {
-        postfix += operators[c];
-        console.log("us");
-    }
-    //console.log(operands);
-    //console.log(postfix);
-    return postfix;
+            operands.splice(2);
+         }
+      } else {
+      }
+   }
+   for (let c = 0; c < operators.length; c++) {
+      if (Weight(operators[c]) > 1) {
+         postfix += operators[c];
+         operators.splice(operators.indexOf(operators[c]), 1);
+      }
+   }
+   for (let c = 0; c < operators.length; c++) {
+      postfix += operators[c];
+   }
+   return solvePostfix(postfix);
 }
 function Weight(str) {
-    switch (str) {
-        case "*":
-            return 2;
-        case "/":
-            return 2;
-        case "+":
-            return 1;
-        case "-":
-            return 1;
-    }
+   switch (str) {
+      case "*":
+         return 2;
+      case "/":
+         return 2;
+      case "+":
+         return 1;
+      case "-":
+         return 1;
+   }
+}
+function solvePostfix(array) {
+   var stack = array.split("");
+   var ahoj = [];
+   var result = 0;
+   for (let i = 0; i < array.length; i++) {
+      if (isNaN(stack[i])) {
+         var a = ahoj.pop();
+         var b = ahoj.pop();
+         switch (stack[i]) {
+            case "*":
+               result = parseInt(a) * parseInt(b);
+               break;
+            case "+":
+               result = parseInt(a) + parseInt(b);
+               break;
+            case "/":
+               result = parseInt(a) / parseInt(b);
+               break;
+            case "-":
+               result = parseInt(b) - parseInt(a);
+               break;
+         }
+         ahoj.push(result);
+      } else {
+         ahoj.push(stack[i]);
+      }
+   }
+   return ahoj[0];
 }
 var input = document.getElementById("vyraz");
 input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("vysledek").innerHTML =
-            "Vysledek je:" + solveExpression();
-    }
+   event.preventDefault();
+   if (event.keyCode === 13) {
+      document.getElementById("vysledek").innerHTML =
+         "Vysledek je:" + solveExpression();
+   }
 });
